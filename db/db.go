5308ca29765a800b2dc69a0a52f245ff19fede7c
nozomi0966 <nozomotoitech@gmail.com>
2018-09-28 23:51:41 +0900

    interfaceを定義

--- db/db.go
+++ db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/nozo-moto/search_engine/page"
 	"github.com/pkg/errors"
 )
 
@@ -10,37 +11,50 @@ const (
 	driverName = "mysql"
 )
 
-type Page struct {
+type PageMySQLAdapter struct {
 	ID      int64  `db:"ID"`
 	URL     string `db:"URL"`
 	Content string `db:"CONTENT"`
 	DB      *sqlx.DB
 }
 
+func NewPageMySQLAdapter(db *sqlx.DB) *PageMySQLAdapter {
+	return &PageMySQLAdapter{
+		DB: db,
+	}
+}
+
+func (p *PageMySQLAdapter) domain() *page.Page {
+	return &page.Page{
+		ID:      p.ID,
+		URL:     p.URL,
+		Content: p.Content,
+	}
+}
+
 func ConnectToDatabase() (*sqlx.DB, error) {
-	db, err := sqlx.Connect(driverName, "")
+	db, err := sqlx.Connect(driverName, "root:password@tcp(0.0.0.0:13306)/search_engine")
 	if err != nil {
-		return db, err
+		return nil, err
 	}
 	return db, nil
 }
 
-func (p *Page) Insert() (*Page, error) {
-
-	stmt, err := p.DB.Prepare(`INSERT INTO Page (URL, CONTENT) VALUES (?, ?)`)
+func (p *PageMySQLAdapter) Add(page *page.Page, dbx *sqlx.DB) (*page.Page, error) {
+	stmt, err := dbx.Prepare(`INSERT INTO Page (URL, CONTENT) VALUES (?, ?)`)
 	defer stmt.Close()
 	if err != nil {
-		return p, errors.Wrap(err, "page insert error")
+		return nil, errors.Wrap(err, "page insert error")
 	}
 	result, err := stmt.Exec(p.URL, p.Content)
 	if err != nil {
-		return p, errors.Wrap(err, "stmt exec error")
+		return nil, errors.Wrap(err, "stmt exec error")
 	}
 	id, err := result.LastInsertId()
 	if err != nil {
-		return p, errors.Wrap(err, "last insertid error")
+		return nil, errors.Wrap(err, "last insertid error")
 	}
 	p.ID = id
 
-	return p, nil
+	return p.domain(), nil
 }
--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/nozo-moto/search_engine/db"
+)
+
 func main() {
+	dbx, err := db.ConnectToDatabase()
+	if err != nil {
+		panic(err)
+	}
+	page := &db.Page{
+		URL:     "http://example.com",
+		Content: "test",
+		DB:      dbx,
+	}
+
+	page, err = page.Insert()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(page)
+
 }
