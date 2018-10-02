8e3e5f38d8d3c0a1647c181b9e52f5ae6666d342
nozomi0966 <nozomotoitech@gmail.com>
2018-10-02 16:22:25 +0900

    impl search box and show table

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-   </div>
+    <Main />
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Main from "./components/Main.vue";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Main
   }
 };
 </script>
